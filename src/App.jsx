baf5f826772fa9b5114fea68ca9a624b6b3be282
OrufbekM <orufbekmaxmudjonov77@gmail.com>
2026-05-21 15:29:08 +0500

    Add initial project setup with React, Vite, and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+
+const App = () => {
+  return (
+    <div>
+    <Button variant="outline">Bosish</Button>
+    </div>
+  )
+}
+
+export default App
